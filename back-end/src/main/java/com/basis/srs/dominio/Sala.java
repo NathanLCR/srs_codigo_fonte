2473dda9c5df8759a2b247ee56e6e689a6036ddf
Pedro Arthur <pedro15cg66@gmail.com>
2020-10-05 15:04:28 -0300

    Salas Restructured

--- back-end/src/main/java/com/basis/srs/dominio/Sala.java
+++ back-end/src/main/java/com/basis/srs/dominio/Sala.java
@@ -2,7 +2,6 @@ package com.basis.srs.dominio;
 
 import lombok.Getter;
 import lombok.Setter;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
--- back-end/src/main/java/com/basis/srs/servico/mapper/SalaMapper.java
+++ back-end/src/main/java/com/basis/srs/servico/mapper/SalaMapper.java
@@ -2,7 +2,6 @@ package com.basis.srs.servico.mapper;
 
 import com.basis.srs.dominio.Sala;
 import com.basis.srs.servico.dto.SalaDto;
-import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring", uses = {})
 public interface SalaMapper extends EntityMapper<SalaDto, Sala>{
