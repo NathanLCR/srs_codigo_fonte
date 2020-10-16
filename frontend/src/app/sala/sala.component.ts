9d7c1c0b3dc1bebe3c7a04574a8797522a6b840c
Pedro Arthur <pedro15cg66@gmail.com>
2020-10-16 16:03:14 -0300

    Reajuste de Post-sala - Front

--- frontend/src/app/sala/sala.component.ts
+++ frontend/src/app/sala/sala.component.ts
@@ -1,8 +1,10 @@
+import { EquipamentoService } from './../equipamento/equipamento.service';
 import { SalaService } from './sala.service';
 import { ConfirmationService } from 'primeng/api';
 import { Component, OnInit } from '@angular/core';
 import Sala from '../models/Sala';
 import { FormBuilder } from '@angular/forms';
+import Equipamento from '../models/Equipamento';
 
 @Component({
   selector: 'app-sala',
@@ -18,9 +20,35 @@ export class SalaComponent implements OnInit {
 
   salaForm;
 
+  equipamentos;
+
+  tiposDeSala = [
+    {
+        label: "Sala de Reunião",
+        value: 1,
+    },
+    {
+        label: "Sala de Trabalho",
+        value: 2,
+    },
+    {
+        label: "Sala de Vídeo",
+        value: 3,
+    },
+    {
+      label: "Sala de Palestras",
+      value: 4,
+    },
+    {
+      label: "Auditório",
+      value: 5,
+    },
+];
+
   constructor(private salaService: SalaService, 
     private formBuilder: FormBuilder,
-    private confirmationService: ConfirmationService
+    private confirmationService: ConfirmationService,
+    private equipamentoService: EquipamentoService,
     ) { 
       this.salaForm = this.formBuilder.group({
         precoDiaria: "",
@@ -36,20 +64,85 @@ export class SalaComponent implements OnInit {
     this.salaService.getSalas().subscribe(resultado => {
       this.salas = resultado;
     });
+
+    this.equipamentoService.getEquipamentos().subscribe(resulta => {this.equipamentos = resulta.map(e=>{return {label: e.nome, value:e}})}
+    );
   }
 
-  deletar(sala) {
+  deletar(sala) {                                                                         
     this.salaService.deleteSala(sala.id).subscribe();
     this.salas = this.salas.filter(val => val.id !== sala.id);
   }
 
   criar(sala) {
+    console.log(sala)
     this.salaService.postSala(sala).subscribe(); 
   }
 
+  atualizar(sala) {
+    this.salaService.putSala(sala).subscribe();
+  }
+
   showForm() {
+
     this.displayForm = true;
   }
+
+  getTipoSala(sala) {
+    const { label } = this.tiposDeSala.find(
+        (t) => t.value == sala.idTipoEquipamento
+    );
+
+    sala.tipoSala = label;
+    return sala;
+}
+
+  handleSubmit(value) {
+    console.log(value);
+    this.salaService.postSala(value).subscribe();
+    value = this.getTipoSala(value);
+    if (!value.id) {
+        this.salas.push(value);
+    } else {
+        const index = this.salas.findIndex((e) => e.id === value.id);
+        this.salas[index] = value;
+    }
+    this.displayForm = false;
+
+    this.salaForm.reset();
+}
+
+handleEdit(sala) {
+  this.salaForm.setValue({
+      id: sala.id,
+      precoDiaria: sala.precoDiaria,
+      descricao: sala.descricao,
+      capacidade: sala.capacidade,
+      disponivel: sala.disponivel,
+      idTipoSala: sala.idTipoSala,
+      equipamentos: sala.equipamentos,
+  });
+}
+
+
+handleDelete(sala) {
+  this.confirmationService.confirm({
+      message:
+          "Tem certeza que desejar excluir a sala " +
+          sala.descricao,
+      header: "Confirmar exclusão",
+      icon: "pi pi-exclamation-triangle",
+      accept: () => {
+          this.salaService
+              .deleteSala(sala.id)
+              .subscribe();
+          this.salas = this.salas.filter(
+              (val) => val.id !== sala.id
+          );
+      },
+  });
+}
+
 
   onSubmit(){}
 }
