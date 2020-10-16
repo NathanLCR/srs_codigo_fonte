9f5127ebd3479ca592a7b19dd9b92b550d2b7bd4
Nathan Rocha <nathan.rocha@maisunifacisa.com.br>
2020-10-16 13:47:01 -0300

    Crud

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,20 +1,22 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { DiarioErrosComponent } from './components/diario-erros/diario-erros.component';
-import { LoginSuccessComponent } from '@nuvem/angular-base';
-import {EquipamentoComponent} from './equipamento/equipamento.component';
-import { ReservaComponent } from './reserva/reserva.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { DiarioErrosComponent } from "./components/diario-erros/diario-erros.component";
+import { LoginSuccessComponent } from "@nuvem/angular-base";
+import { EquipamentoComponent } from "./equipamento/equipamento.component";
+import { ReservaComponent } from "./reserva/reserva.component";
 
 const routes: Routes = [
-    { path: 'diario-erros', component: DiarioErrosComponent, data: { breadcrumb: 'Diário de Erros'} },
-    { path: 'login-success', component: LoginSuccessComponent },
-    { path: 'equipamentos', component: EquipamentoComponent },
-    { path: 'reservas', component: ReservaComponent}
-
+    {
+        path: "diario-erros",
+        component: DiarioErrosComponent,
+        data: { breadcrumb: "Diário de Erros" },
+    },
+    { path: "login-success", component: LoginSuccessComponent },
+    { path: "equipamentos", component: EquipamentoComponent },
+    { path: "reservas", component: ReservaComponent },
+];
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes)
-  ],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
