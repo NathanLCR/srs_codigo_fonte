f07121805f65c00672814af450ba84e4ad3229aa
felipemalheiros <felipemalheirosb@gmail.com>
2020-10-28 14:45:16 -0300

    Ajustes

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import Cliente from "../models/Cliente";
 import { ClienteService } from "../cliente/cliente.service";
-import { ClienteComponent } from '../cliente/cliente.component';
 import { BarGrafico } from './BarGrafico';
+import Equipamento from '../models/Equipamento';
+import Reserva from '../models/Reserva';
+import Sala from '../models/Sala';
+import { EquipamentoService } from '../equipamento/equipamento.service';
+import { SalaService } from '../sala/sala.service';
+import { ReservaService } from '../reserva/reserva.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -13,13 +18,22 @@ export class DashboardComponent implements OnInit {
 
   
   clientes: Cliente[];
+  equipamentos: Equipamento[];
+  salas: Sala[];
+  reservas: Reserva[];
+  
   data: BarGrafico;
-  numClientes: any = 1;
   
   constructor(
        private clienteService: ClienteService,
+       private equipamentoService: EquipamentoService,
+       private salaService: SalaService,
+       private reservaService: ReservaService
        
-  ) { }
+  ) { 
+    
+    this.data = new BarGrafico().data
+  }
 
 
   ngOnInit(): void {
@@ -27,6 +41,18 @@ export class DashboardComponent implements OnInit {
       this.clienteService.getClientes().subscribe((resultado) => {
            this.clientes = resultado;
        });
+
+       this.equipamentoService.getEquipamentos().subscribe((resultado) => {
+        this.equipamentos = resultado;
+      });
+
+      this.salaService.getSalas().subscribe((resultado) => {
+        this.salas = resultado;
+      });
+
+      this.reservaService.getReservas().subscribe((resultado) => {
+        this.reservas = resultado;
+      });
        
       }
      
